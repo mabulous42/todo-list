2cc4f764a5d7a128a11bbdccd77afb9d9da69653
Abbas <tundeeey@gmail.com>
2023-04-04 17:20:00 +0100

    I edited the codes not to add empty string to the array

--- script.js
+++ script.js
@@ -6,29 +6,32 @@ let showTodo = document.getElementById('list');
 
 let arrNew = [];
 function showBtn() {
-    // arrNew = reveal.value;
-    showTodo.innerHTML = "";
-    arrNew.push(reveal.value);
-    arrNew.map((sh, rem) => {
-        console.log(sh, rem);
-        showTodo.innerHTML +=`
+    if (reveal.value === "") {
+        return
+    } else {
+        showTodo.innerHTML = "";
+        arrNew.push(reveal.value);
+        arrNew.map((sh, rem) => {
+            console.log(sh, rem);
+            showTodo.innerHTML += `
         <div class="dis">
             <h1>${sh}</h1>
             <button id='d-btn' onclick='del(${rem})'>Delete</button>
         </div>
         `
-    })
-    // list.innerHTML += "<h1 id='dis'>" + arrNew + "<br>" + "<button id='d-btn' onclick='del('rem')'>"+"Delete"+"</button>"+"</h1>";
-    
+        })
+    }
+
+
 }
 
 function del(rem) {
-    arrNew.splice(rem,1);
-    
+    arrNew.splice(rem, 1);
+
     showTodo.innerHTML = "";
     arrNew.map((sh, rem) => {
         console.log(sh, rem);
-        showTodo.innerHTML +=`
+        showTodo.innerHTML += `
         <div class="dis">
             <h1>${sh}</h1>
             <button id='d-btn' onclick='del(${rem})'>Delete</button>
