c8c791008180aabee23f410c23642d9c02efcfc4
Abbas <tundeeey@gmail.com>
2023-04-05 11:01:32 +0100

    I just added a new div that has another input and button for edit and also added a status message to know if the input was added to the Todo list successfully or not

--- script.js
+++ script.js
@@ -1,75 +1,63 @@
 let input = document.getElementById("input");
 let showTodo = document.getElementById('display');
+let showStatusMessage = document.getElementById('showStatusMessage');
 
 let arrayTodo = [];
-function addItems() {
-    if (input.value === "") {
-        alert("Input Something");
-        return;
-    }
-    else {
-        if (arrayTodo.includes(input.value)) {
-            alert("Items already exist");
-            input.value = "";
-            return;
-        }
-        else {
-            showTodo.innerHTML = "";
-            arrayTodo.push(input.value);
-            arrayTodo.map((arrayElement, index) => {
-                showTodo.innerHTML += `            
+
+function printArray() {
+    showTodo.innerHTML = "";
+    arrayTodo.map((arrayElement, index) => {
+        showTodo.innerHTML += `            
         <div class="display">
             <h1 id='arrayItem'>${arrayElement}</h1>
             <div>
-            <button id='delete-btn' onclick='edit(${index})'>Edit</button>
-            <button id='delete-btn' onclick='del(${index})'>Delete</button>
-            </div>
-        </div>
-        `
-            })
-            input.value = "";
-        }
-
-    }
-}
-
-function del(rem) {
-    arrayTodo.splice(rem, 1);
-    showTodo.innerHTML = "";
-    arrayTodo.map((sh, rem) => {
-        console.log(sh, rem);
-        showTodo.innerHTML += `
-        <div class="display">
-            <h1>${sh}</h1>
-            <div>
-                <button id='delete-btn' onclick='edit(${rem})'>Edit</button>
-                <button id='delete-btn' onclick='del(${rem})'>Delete</button>
+                <button id='edit-btn' onclick='edit(${index})'>Edit</button>
+                <button id='delete-btn' onclick='del(${index})'>Delete</button>
             </div>
         </div>
         `
     })
 }
 
+function addItems() {
+    if (input.value === "") {
+        // alert("Input field cannot be empty");
+        showStatusMessage.innerHTML = "<p id='error-message'>"+"Input field cannot be empty!!!"+"</p>";
+        return;
+    }
+    else {
+        arrayTodo.push(input.value);
+        showStatusMessage.innerHTML = "<p id='success-message'>"+"Todo added successfully"+"</p>";
+        printArray();
+        input.value = "";
+        // if (arrayTodo.includes(input.value)) {
+        //     alert("Items already exist");
+        //     input.value = "";
+        //     return;
+        // }
+        // else {
+        //     showTodo.innerHTML = "";
+        //     arrayTodo.push(input.value);
+        //     printArray();
+        //     input.value = "";
+        // }
+
+    }
+}
+
+function del(rem) {
+    arrayTodo.splice(rem, 1);
+    printArray();
+}
+
 function edit(rem) {
     if (input.value === "") {
-        alert("Input Something");
+        alert("Input field cannot be empty");
         return;
     }
     else {
         arrayTodo.splice(rem, 1, input.value);
-        showTodo.innerHTML = "";
-        arrayTodo.map((sh, rem) => {
-            console.log(sh, rem);
-            showTodo.innerHTML += `
-        <div class="display">
-            <h1>${sh}</h1>
-            <div>
-                <button id='delete-btn' onclick='edit(${rem})'>Edit</button>
-                <button id='delete-btn' onclick='del(${rem})'>Delete</button>
-            </div>
-        </div>
-        `
-        })
+        printArray();
         input.value = "";
     }
 
