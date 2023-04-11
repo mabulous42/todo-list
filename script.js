f1c6aab5ef182d3472da77b6aa74e36b3578daba
Abbas <tundeeey@gmail.com>
2023-04-11 10:31:01 +0100

    I added a local storage to save all my todo list before a refresh on my browser

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function printArray() {
     arrayTodo.map((arrayElement, index) => {
         showTodo.innerHTML += `            
         <div class="display">
-            <h1 id='arrayItem'>${index+1} ${" "} ${arrayElement}</h1>
+            <h1 id='arrayItem'>${index + 1} ${" "} ${arrayElement}</h1>
             <div>
                 <button id='edit-btn' onclick='edit(${index})'>Edit</button>
                 <button id='delete-btn' onclick='del(${index})'>Delete</button>
@@ -23,13 +23,20 @@ function printArray() {
     })
 }
 
+
+let locStorage = [];
 function addItems() {
+
     if (input.value === "") {
         showStatusMessage.innerHTML = "<p id='error-message'>" + "Input field cannot be empty!!!" + "</p>";
 
         return;
     }
     else {
+        // localStorage.setItem("count", input.value)
+        locStorage.push(input.value); //Add the text 'item1' to locStorage
+        localStorage.setItem('todoList', JSON.stringify(locStorage));
+
         arrayTodo.push(input.value);
         showStatusMessage.innerHTML = "<p id='success-message'>" + "Todo added successfully" + "</p>";
         printArray();
@@ -37,12 +44,20 @@ function addItems() {
     }
 }
 
+function getLocalStorage() {
+    let getter = localStorage.getItem("todoList");
+    console.log(getter);
+}
+getLocalStorage();
+
 function del(rem) {
     arrayTodo.splice(rem, 1);
     printArray();
 }
 
 let editIndex = 0;
+
+
 
 function edit(rem) {
     editIndex = rem;
@@ -60,10 +75,14 @@ function done() {
     }
     else {
         arrayTodo.splice(editIndex, 1, editInput.value);
+
+        locStorage.splice(editIndex, 1, editInput.value); //Add the text 'item1' to locStorage
+        localStorage.setItem('todoList', JSON.stringify(locStorage));
+        
         printArray();
         editInput.value = "";
         showInputContainer.style.display = "block";
-        showEditBox.style.display = "none";  
+        showEditBox.style.display = "none";
     }
 
 }
