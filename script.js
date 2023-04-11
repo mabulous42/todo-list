17d3b123eae7ac9a5e36548b16824e5c038718dd
Abbas <tundeeey@gmail.com>
2023-04-11 22:30:24 +0100

    changed the edit function parameter name from rem to index

--- script.js
+++ script.js
@@ -9,18 +9,7 @@ let arrayTodo = [];
 showEditBox.style.display = "none";
 
 function printArray() {
-    showTodo.innerHTML = "";
-    arrayTodo.map((arrayElement, index) => {
-        showTodo.innerHTML += `            
-        <div class="display">
-            <h1 id='arrayItem'>${index + 1} ${" "} ${arrayElement}</h1>
-            <div>
-                <button id='edit-btn' onclick='edit(${index})'>Edit</button>
-                <button id='delete-btn' onclick='del(${index})'>Delete</button>
-            </div>
-        </div>
-        `
-    })
+    OverallDisplay(arrayTodo);
 }
 
 
@@ -44,27 +33,45 @@ function addItems() {
     }
 }
 
+let getter = ""
 function getLocalStorage() {
-    let getter = localStorage.getItem("todoList");
+    getter = JSON.parse(localStorage.getItem("todoList"));
     console.log(getter);
+    OverallDisplay(getter);
 }
 getLocalStorage();
 
+function OverallDisplay(arrayName) {
+    showTodo.innerHTML = "";
+    arrayName.map((arrayElement, index) => {
+        showTodo.innerHTML += `            
+        <div class="display">
+            <h1 id='arrayItem'>${index + 1} ${" "} ${arrayElement}</h1>
+            <div>
+                <button id='edit-btn' onclick='edit(${index})'>Edit</button>
+                <button id='delete-btn' onclick='del(${index})'>Delete</button>
+            </div>
+        </div>
+        `
+    })
+}
+
 function del(rem) {
     arrayTodo.splice(rem, 1);
+    locStorage.splice(rem, 1); //Add the text 'item1' to locStorage
+    localStorage.setItem('todoList', JSON.stringify(locStorage));
+
     printArray();
 }
 
 let editIndex = 0;
 
-
-
-function edit(rem) {
-    editIndex = rem;
-    console.log(rem);
+function edit(index) {
+    editIndex = index;
+    console.log(index);
     editInput.value = "";
     showEditBox.style.display = "block";
-    editInput.value = arrayTodo[rem];
+    editInput.value = arrayTodo[index];
     showInputContainer.style.display = "none";
 }
 
@@ -78,7 +85,7 @@ function done() {
 
         locStorage.splice(editIndex, 1, editInput.value); //Add the text 'item1' to locStorage
         localStorage.setItem('todoList', JSON.stringify(locStorage));
-        
+
         printArray();
         editInput.value = "";
         showInputContainer.style.display = "block";
